b9ba187a53110518a0bf0db9465eb6935d863834
Bl1xvan <cflavien1@my.brookdalecc.edu>
2021-11-01 15:47:03 -0400

    styled fontconts

--- index.php
+++ index.php
@@ -10,10 +10,10 @@
 </head>
 <body>
     <div class="row">
-    <div class="col-sm-6 text-center bg-primary">
-        <div class="form-cont">
+    <div class="col-sm-6">
+        <div class="formcont">
             <form action="/action.php">
-                <h2>Login</h2>
+                <h2 class="text-center">Login</h2>
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input type="email" id="email" class="form-control" placeholder="Enter Email" name="email">
@@ -25,14 +25,14 @@
                 <div class="checkbox">
                     <label><input type="checkbox" name="remember">Remember me</label>
                 </div>
-                <button type="submit" class="btn btn-default">
+                <button type="submit" class="btn btn-default">Submit</button>
             </form>
         </div>
     </div>
-    <div class="col-sm-6 text-center bg-info">
-        <div class="form-cont">
+    <div class="col-sm-6">
+        <div class="formcont">
             <form action="/action.php">
-                <h2>Sign Up</h2>
+                <h2 class="text-center">Sign Up</h2>
                 <div class="form-group">
                     <label for="email">Email</label>
                     <input type="email" id="email" class="form-control" placeholder="Enter Email" name="email">
@@ -45,7 +45,7 @@
                     <label for="password">Re-enter Password</label>
                     <input type="password" id="pwd" class="form-control" placeholder="Re-enter Password" name="pwd">
                 </div>
-                <button type="submit" class="btn btn-default">
+                <button type="submit" class="btn btn-default">Submit</button>
             </form>
         </div>
     </div>
