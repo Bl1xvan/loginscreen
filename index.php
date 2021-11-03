ea178f61e5149343fbab7b90c079fa96f4215843
Bl1xvan <cflavien1@my.brookdalecc.edu>
2021-11-03 17:12:17 -0400

    added config and mysqli connection

--- index.php
+++ index.php
@@ -10,45 +10,50 @@
 </head>
 <body>
     <div class="row">
-    <div class="col-sm-6">
-        <div class="formcont">
-            <form action="/action.php">
-                <h2 class="text-center">Login</h2>
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" id="email" class="form-control" placeholder="Enter Email" name="email">
-                </div>
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" id="pwd" class="form-control" placeholder="Enter Password" name="pwd">
-                </div>
-                <div class="checkbox">
-                    <label><input type="checkbox" name="remember">Remember me</label>
-                </div>
-                <button type="submit" class="btn btn-default">Submit</button>
-            </form>
+        <div class="page-header text-center">
+            <h1>WELCOME</h1>
         </div>
     </div>
-    <div class="col-sm-6">
-        <div class="formcont">
-            <form action="/action.php">
-                <h2 class="text-center">Sign Up</h2>
-                <div class="form-group">
-                    <label for="email">Email</label>
-                    <input type="email" id="email" class="form-control" placeholder="Enter Email" name="email">
-                </div>
-                <div class="form-group">
-                    <label for="password">Password</label>
-                    <input type="password" id="pwd" class="form-control" placeholder="Enter Password" name="pwd">
-                </div>
-                <div class="form-group">
-                    <label for="password">Re-enter Password</label>
-                    <input type="password" id="pwd" class="form-control" placeholder="Re-enter Password" name="pwd">
-                </div>
-                <button type="submit" class="btn btn-default">Submit</button>
-            </form>
+    <div class="row">
+        <div class="col-sm-6">
+            <div class="formcont" id="login">
+                <form action="/action.php">
+                    <h2 class="text-center">Login</h2>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" class="form-control" placeholder="Enter Email" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" id="pwd" class="form-control" placeholder="Enter Password" name="pwd">
+                    </div>
+                    <div class="checkbox">
+                        <label><input type="checkbox" name="remember">Remember me</label>
+                    </div>
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </form>
+            </div>
         </div>
-    </div>
+        <div class="col-sm-6">
+            <div class="formcont" id="signup">
+                <form action="/action.php">
+                    <h2 class="text-center">Sign Up</h2>
+                    <div class="form-group">
+                        <label for="email">Email</label>
+                        <input type="email" id="email" class="form-control" placeholder="Enter Email" name="email">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="password" id="pwd" class="form-control" placeholder="Enter Password" name="pwd">
+                    </div>
+                    <div class="form-group">
+                        <label for="password">Re-enter Password</label>
+                        <input type="password" id="pwd" class="form-control" placeholder="Re-enter Password" name="pwd">
+                    </div>
+                    <button type="submit" class="btn btn-default">Submit</button>
+                </form>
+            </div>
+        </div>
     </div>
 </body>
 </html>
